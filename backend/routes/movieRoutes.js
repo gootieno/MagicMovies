86322ce0dca866a7bb8ba0f54c29087f812f90d5
gootieno <geoff.otieno7@gmail.com>
2024-02-05 21:02:15 -0800

    initialized sequelize and sequelize-cli

--- backend/routes/movieRoutes.js
+++ backend/routes/movieRoutes.js
@@ -1,10 +1,7 @@
-// src/routes/moviesRoutes.js
 const express = require("express");
 const router = express.Router();
 
-// Example route for getting upcoming movies
 router.get("/upcoming", (req, res) => {
-  // Logic to fetch and return upcoming movies
   res.json([{ id: 1, title: "Upcoming Movie", release_date: "2023-12-25" }]);
 });
 
